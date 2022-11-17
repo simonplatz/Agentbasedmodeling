19d868a0a80d351cac0ece9a54fde5c028e3a045
LucasAjander <luaja19@student.sdu.dk>
2022-11-17 10:08:52 +0100

    Update agent.jl

--- agent.jl
+++ agent.jl
@@ -1,7 +1,7 @@
 using Agents
 
 @agent Trader NoSpaceAgent begin  
-  balance::Int # how 
+  balance::Int # how much money the agent has
   sell_buy::Float64 # whether the agent will sell, buy or hold
   volatilityScore::Float64
 end
