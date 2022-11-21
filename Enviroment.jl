34bedfafa5a8bd9e75cdfe7f3975bfd99554e9d5
Peter Heilbo Ratgen <peter@pratgen.dk>
2022-11-22 00:04:19 +0100

    Work
    Missing a "get agent which fits some value"

--- Enviroment.jl
+++ Enviroment.jl
@@ -36,7 +36,7 @@ function initialize(; numagents = 50, price = 100, totalCoinAmount = 10000)
 
   balance = totalCoinAmount/numagents # start all with equal balance
   sell_buy = 0.5 # the initial sell/buy is neutral
-  volatilityScore = Random.rand(Float64) # randomly assign how volatile the trader should be 
+  volatilityScore = Random.rand(Float64) * 2 # randomly assign how volatile the trader should be 
 
   for n in 1:numagents
     agent = TradingAgent(n, balance, sell_buy, volatilityScore)
@@ -56,13 +56,16 @@ function trader_step!(agent, model)
   # detemine sell_buy
   
   if (model.news < 0.33) # negative news
-    agent.sell_buy = agent.sell_buy/2
+    agent.sell_buy = agent.sell_buy/( 1.5 * agent.volatilityScore)
   else if (model.news > 0.66) # positive news
-    agent.sell_buy = agent.sell_buy*2
+      agent.sell_buy = agent.sell_buy* (1.5 * agent.volatilityScore)
   end
 
+  if (agent.sell_buy < 0.33) ## sell
+    model.
+  else if (agent.sell_buy > 0.66) ## buy
 
-
+  end
 
 end
 
