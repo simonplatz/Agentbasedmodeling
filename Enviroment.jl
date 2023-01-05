bbad351feddc13e507ae886bb0e2be66ddd8acbc
niklas2801 <55789806+niklas2801@users.noreply.github.com>
2023-01-05 15:06:54 +0100

    asdasdasd

--- Enviroment.jl
+++ Enviroment.jl
@@ -6,6 +6,8 @@ mutable struct TradingAgent <: AbstractAgent
   balance::Float64 # how much money the agent has
   sell_buy::Float64 # whether the agent will sell, buy or hold
   volatilityScore::Float64
+  coinsOwned::Float64
+  startBalance::Int
 end
 
 
@@ -16,16 +18,17 @@ end
 
 # Scheduler which generates news events
 function (ms::MarketScheduler)(model::ABM)
-  println(model)
+  #println(model)
   ms.news = Random.rand(Float64)
 
   return allids(model)
 end
 
-function initialize(; numagents=2, price=100, totalCoinAmount=10000)
+function initialize(; numagents=20, price=100.0, totalCoinAmount=10000, coinLeft=10000)
   properties = Dict(
     :price => price, # initial price
     :totalCoinAmount => totalCoinAmount, # how many coins exist
+    :coinLeft => coinLeft
   )
 
   ms = MarketScheduler(0.5)
@@ -36,27 +39,38 @@ function initialize(; numagents=2, price=100, totalCoinAmount=10000)
     properties=properties, scheduler=ms
   )
 
-  balance = totalCoinAmount / numagents # start all with equal balance
-  sell_buy = 0.7 # the initial sell/buy is neutral
+  balance = 10000 # start all with equal balance
+  sell_buy = 0.5 # the initial sell/buy is neutral
   volatilityScore = Random.rand(Float64) * 2 # randomly assign how volatile the trader should be 
+  coinsOwned = 0
+  startBalance = balance
 
   for n in 1:numagents
-    agent = TradingAgent(n, balance, sell_buy, volatilityScore)
+    agent = TradingAgent(n, balance, sell_buy, volatilityScore, coinsOwned, startBalance)
     add_agent!(agent, model)
   end
 
   return model
 end
 
-function buy(model, usablebalance)
+
+function buy(model, usablebalance, agent)
+
   amount = 0
-  while usablebalance % amount == 0 && amount < 10
+  while (usablebalance - (amount * model.price)) > 0 && amount < 10
     amount += 1
   end
-  if amount < maxCoinsPerTrade
-    priceIncreasePercentage = amount / 10
-    global model.price += model.price * priceIncreasePercentage
 
+  if amount <= maxCoinsPerTrade && agent.balance - model.price > 0
+    priceIncreasePercentage = amount / 10
+    usablebalance -= amount * model.price
+    println("Price before ", model.price)
+    model.price += model.price * priceIncreasePercentage
+    agent.coinsOwned += amount
+    model.coinLeft -= amount
+    agent.balance += usablebalance
+
+    println("Price after ", model.price)
     return
 
   else
@@ -67,25 +81,53 @@ function buy(model, usablebalance)
   return
 end
 
+function sell(model, agent)
+  coinsSold = 0
+  if model.price < 500 && agent.balance > 100
+    coinsSold = agent.coinsOwned * 0.10
+    agent.coinsOwned = agent.coinsOwned - coinsSold
+    agent.balance = coinsSold * model.price
+  elseif model.price > 500 && agent.balance > 100
+    coinsSold = coinsOwned / 2
+    agent.coinsOwned = agent.coinsOwned - coinsSold
+    agent.balance = coinsSold * model.price
+  elseif agent.balance <= 100
+    coinsSold = agent.coinsOwned * 0.9
+    agent.coinsOwned = agent.coinsOwned - coinsSold
+    agent.balance = coinsSold * model.price
+  end
+
+  return
+end
 
 tradesPerTick = 1
 maxCoinsPerTrade = 10
 wantToBuy = 9
 amount = 10
 function trader_step!(agent, model)
-  println("running trader_step")
+  #println("running trader_step")
   # detemine sell_buy
-  usablebalance = agent.balance / 5
-  agent.balance = agent.balance - 100.0
+  if agent.balance - model.price > 0
+    usablebalance = agent.balance / 5
+    agent.balance = agent.balance - usablebalance
+  else
+    usablebalance = agent.balance
+    agent.balance = agent.balance - usablebalance
+  end
+
   if (model.scheduler.news < 0.33) # negative news
     agent.sell_buy = agent.sell_buy / (1.5 * agent.volatilityScore)
   elseif (model.scheduler.news > 0.66) # positive news
     agent.sell_buy = agent.sell_buy * (1.5 * agent.volatilityScore)
   end
 
+  if agent.balance > 3 * agent.startBalance
+    agent.sell_buy = agent.sell_buy * 2
+  end
   if (agent.sell_buy < 0.33) ## sellq
+    sell(model, agent)
   elseif (agent.sell_buy > 0.66) ## buy
-    buy(model, usablebalance)
+    buy(model, usablebalance, agent)
 
   end
 
@@ -93,9 +135,9 @@ end
 
 model = initialize()
 
-step!(model, trader_step!, 2)
+step!(model, trader_step!, 100)
 
-println(random_agent(model))
+println(Agents.allagents(model))
 
 
 
