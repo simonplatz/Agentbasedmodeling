d1f5c81bd27d384a14e766323e92274582b7817a
niklas2801 <55789806+niklas2801@users.noreply.github.com>
2023-01-05 12:11:14 +0100

    aw

--- Enviroment.jl
+++ Enviroment.jl
@@ -18,9 +18,10 @@ end
 function (ms::MarketScheduler)(model::ABM)
   println(model)
   ms.news = Random.rand(Float64)
+
 end
 
-function initialize(; numagents = 50, price = 100, totalCoinAmount = 10000)
+function initialize(; numagents=2, price=100, totalCoinAmount=10000)
   properties = Dict(
     :price => price, # initial price
     :totalCoinAmount => totalCoinAmount, # how many coins exist
@@ -30,12 +31,12 @@ function initialize(; numagents = 50, price = 100, totalCoinAmount = 10000)
 
   # the space is nothing by default
   model = ABM(
-    TradingAgent; 
-    properties = properties, scheduler = ms
+    TradingAgent;
+    properties=properties, scheduler=ms
   )
 
-  balance = totalCoinAmount/numagents # start all with equal balance
-  sell_buy = 0.5 # the initial sell/buy is neutral
+  balance = totalCoinAmount / numagents # start all with equal balance
+  sell_buy = 0.7 # the initial sell/buy is neutral
   volatilityScore = Random.rand(Float64) * 2 # randomly assign how volatile the trader should be 
 
   for n in 1:numagents
@@ -44,25 +45,28 @@ function initialize(; numagents = 50, price = 100, totalCoinAmount = 10000)
   end
 
   return model
-end 
+end
 
 
-# tradesPerTick = 1
-# maxCoinsPerTrade = 10
-# wantToBuy = 11
-# amount = 10
-
+tradesPerTick = 1
+maxCoinsPerTrade = 10
+wantToBuy = 9
+amount = 10
+price = 100
 function trader_step!(agent, model)
   # detemine sell_buy
-  
+  println(agent.sell_buy)
+  usablebalance = agent.balance / 5
+  agent.balance = agent.balance - usablebalance
   if (model.news < 0.33) # negative news
-    agent.sell_buy = agent.sell_buy/( 1.5 * agent.volatilityScore)
-  else if (model.news > 0.66) # positive news
-      agent.sell_buy = agent.sell_buy* (1.5 * agent.volatilityScore)
+    agent.sell_buy = agent.sell_buy / (1.5 * agent.volatilityScore)
+  elseif (model.news > 0.66) # positive news
+    agent.sell_buy = agent.sell_buy * (1.5 * agent.volatilityScore)
   end
 
-  if (agent.sell_buy < 0.33) ## sell
-  else if (agent.sell_buy > 0.66) ## buy
+  if (agent.sell_buy < 0.33) ## sellq
+  elseif (agent.sell_buy > 0.66) ## buy
+    buy(model, usablebalance)
 
   end
 
@@ -74,44 +78,51 @@ step!(model, trader_step!, 2)
 
 println(random_agent(model))
 
-function buy(amount, price)
-    ##coinsLeft -= amount
-    if amount < maxCoinsPerTrade
-        priceIncreasePercentage = amount / 10
-        global price += price * priceIncreasePercentage
-        return
+function buy(model, usablebalance)
+  amount = 0
+  while usablebalance % amount == 0 && amount < 10
+    amount += 1
+  end
+  if amount < maxCoinsPerTrade
+    priceIncreasePercentage = amount / 10
+    global model.properties.price += model.properties.price * priceIncreasePercentage
 
-    else
-        println("too many damn coins")
-        return
-
-    end
     return
-end
-function optimization(wantToBuy)
-    if wantToBuy >= 10
-        buy(amount, price)
-        return
-    end
+
+  else
+    println("too many damn coins")
     return
+
+  end
+  return
 end
 
-function priceChange(price)
-    n = Random.rand(Float16)
-    x = Random.rand((0, 1))
-    news = n + x
-    if news < 0.3
-        news = 0.3
-        price = price * news
-        println(news)
-        return price
-    end
-    println(news)
-    global price = price * news
-    return price
-end
+
+#function optimization(wantToBuy)
+#    if wantToBuy >= 10
+#        buy(amount, price)
+#        return
+#    end
+#    return
+#end
+
+#function priceChange(price)
+#    n = Random.rand(Float16)
+#    x = Random.rand((0, 1))
+#    news = n + x
+#    if news < 0.3
+#        news = 0.3
+#        price = price * news
+#        println(news)
+#        return price
+#    end
+#    println(news)
+#    global price = price * news
+#    return price
+#end
+
 #optimization(wantToBuy)
 #stringPrice = string(price)
-# priceChange(price)
-# println("price ", price)
+#priceChange(price)
+# println("price ", model.properties.totalCoinAmount)
 
